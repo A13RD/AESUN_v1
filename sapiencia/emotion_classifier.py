1787db3d43458ed16634ae3a5ea27cf4b0abd8ef
valnolboy <valeriabolnoy@gamil.com>
2024-11-04 18:57:33 -0500

    inicio clase

--- sapiencia/emotion_classifier.py
+++ sapiencia/emotion_classifier.py
@@ -1,18 +1,11 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
-
-# Cargar las variables de entorno desde el archivo .env
-# Especifica la ruta completa del archivo si está fuera de la carpeta del código
-load_dotenv('../.env')  # Asegúrate de ajustar el camino si es necesario
-
-# Obtener la API_KEY desde el archivo .env
+load_dotenv('../.env')
 api_key = os.getenv("API_KEY")
-
-# Configuración de la API de Gemini
 genai.configure(api_key=api_key)
 class IA:
-def generar_respuesta_emocional(input_text):
+     def generar_respuesta_emocional(input_text):
     """
     Genera una respuesta emocional usando Gemini, actuando como un asistente de apoyo.
      Parámetros:
@@ -21,5 +14,7 @@ def generar_respuesta_emocional(input_text):
     Retorna:
     - str: respuesta generada por Gemini.
     """
+
+
 
 
