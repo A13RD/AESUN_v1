46d036174beb3d6fa3bbabfadeeaac077356ea7a
valnolboy <valeriabolnoy@gamil.com>
2024-11-04 19:05:40 -0500

    uso

--- sapiencia/emotion_classifier.py
+++ sapiencia/emotion_classifier.py
@@ -5,16 +5,25 @@ load_dotenv('../.env')
 api_key = os.getenv("API_KEY")
 genai.configure(api_key=api_key)
 class IA:
-     def generar_respuesta_emocional(input_text):
+def generar_respuesta_emocional(input_text):
     """
     Genera una respuesta emocional usando Gemini, actuando como un asistente de apoyo.
-     Parámetros:
+    Parámetros:
     - input_text (str): el texto de entrada que describe el estado emocional o la consulta del usuario.
 
-    Retorna:
+    return:
     - str: respuesta generada por Gemini.
     """
     prompt = f"Actúa como un asistente emocional. Alguien te dice: '{input_text}'. Responde con empatía y apoyo emocional."
+
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+
+    return response.text
+# Ejemplo de uso
+input_text = "Me siento un poco triste y solo últimamente. ¿Qué me aconsejas?"
+respuesta = generar_respuesta_emocional(input_text)
+print(respuesta)
 
 
 
